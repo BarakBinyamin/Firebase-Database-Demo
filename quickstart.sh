1964209b0641438137d3d101e7e068700dfeedcf
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-04-20 14:29:06 -0400

    Add files via upload

--- quickstart.sh
+++ quickstart.sh
@@ -1,10 +1,12 @@
 cd 
 
+cd ~/Firebase-Database-Demo
 if [ "$#" -ne 1 ]; then
     echo "oops, try adding the firebase config file as a parameter"
 fi
 
-sed -i '/GOOBER/{r ${1}' -e 'd}'  demo.html
+printf "'/GOOBER/{r $1' -e 'd}' demo.html" | xargs sed -i
+
 
 firefox demo.html
 
