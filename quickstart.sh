d931d3279ce45a2cc455f5603c88dc3f34e02f86
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-04-21 10:30:52 -0400

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -1,13 +1,20 @@
+#navigate the home directory
 cd 
 
+#first arg
 goober=$1
 
+#navigate the demo directory
 cd ~/Firebase-Database-Demo
+
+#Check number of args
 if [ "$#" -ne 1 ]; then
     echo "oops, try adding the firebase config file as a parameter"
 fi
 
+#insert custom configuration into the demo html
 printf "'/GOOBER/{r $goober' -e 'd}' demo.html" | xargs sed -i -e
 
+#launch demo with firefox
 firefox demo.html
 
