53647c72046b0c07dbd6366c52d505720d0d0758
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2020-05-04 17:40:49 -0400

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -12,7 +12,7 @@ if [ "$#" -ne 1 ]; then
     echo "oops, try adding the firebase config file as a parameter"
 fi
 
-#insert custom configuration into the demo html
+#replace the word "GOOBER" in demo.html with the custom configuration 
 printf "'/GOOBER/{r $goober' -e 'd}' demo.html" | xargs sed -i -e
 
 #launch demo with firefox
